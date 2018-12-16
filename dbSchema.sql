3315e7dbb1d84d136609142160ecf93f2ce306fe
DERMOT COONEY <dermotco@ie.ibm.com>
2018-12-16 22:44:59 +0000

    unused sql file

--- dbSchema.sql
+++
@@ -1,4 +0,0 @@
-USE dermotTest
-create table SERVER (ID CHAR(128) NOT NULL, NAME varchar(255) NOT NULL, PRIMARY KEY (ID))
-
-create table test (ID CHAR(128) NOT NULL, NAME varchar(255) NOT NULL, PRIMARY KEY (ID))
